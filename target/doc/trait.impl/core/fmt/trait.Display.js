f2085e87ead989dd6504615b83dfed9021b1106b
HAL-G1THuB <tan.koro.ring@gmail.com>
2025-04-02 17:41:29 +0900

    Update

--- target/doc/trait.impl/core/fmt/trait.Display.js
+++ target/doc/trait.impl/core/fmt/trait.Display.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["jsompiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"jsompiler/core/struct.Json.html\" title=\"struct jsompiler::core::Json\">Json</a>"]]],["jsompiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"jsompiler/struct.Json.html\" title=\"struct jsompiler::Json\">Json</a>"]]]]);
+    var implementors = Object.fromEntries([["jsompiler",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"jsompiler/struct.Json.html\" title=\"struct jsompiler::Json\">Json</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[273,263]}
+//{"start":57,"fragment_lengths":[262]}
